6b42570a48cea81eceb8f028785a6050548f72d3
cemmbyr <cembyr98@gmail.com>
2022-04-07 22:32:05 +0300

    fix

--- services/index.ts
+++ services/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const productsUrl:any = process.env.REQ_URL
+
 export const getProducts = () => {
-  return axios.post(process.env.REQ_URL, {}, {
+  return axios.post(productsUrl, {}, {
     headers: {
       "X-Access-Token": "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"
     }
